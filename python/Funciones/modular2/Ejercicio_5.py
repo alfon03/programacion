91a8682cfabec0254e2b55e3e609b8b927f488bd
alfon03 <114393129+alfon03@users.noreply.github.com>
2022-11-26 19:12:00 +0100

    Delete Ejercicio_5.py

--- python/Funciones/modular2/Ejercicio_5.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-day = int(input("Introduce un dia: "))
-month = int(input("Introduce un mes: "))
-year = int (input ("Introduce un año: "))
-date(2002, 12, 4).isoweekday() == 3
-print(date)
