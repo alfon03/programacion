12525b2f3a20266652769430218e28fd90fe657b
alfon03 <114393129+alfon03@users.noreply.github.com>
2022-11-29 18:49:28 +0100

    Add files via upload

---
+++ python/Funciones/modular3/Ejercicio_7.py
@@ -0,0 +1,50 @@
+'''
+Created on 29 nov 2022
+
+@author: alfon
+'''
+cadena1=("- ¿Nivel de inglés? \n"
+
+"- Alto \n"
+
+"- Bien. Traduzca 'mirar'.\n"
+
+"- Look.\n"
+
+"- Perfecto. Úselo en una frase.\n"
+
+"- 'Luke', yo soy tu padre.\n"
+
+"- Contratado \n")
+print(cadena1)
+cadena2="master level god leveln "
+cadena3="Pro"
+
+def reemplazarPalabra(cadena1,cadena2,cadena3):
+    nuevafrase=""
+    es=0
+    desplaza=0
+    control=0
+    if cadena2 in cadena1:
+        c=0
+        while c < len(cadena1) and control==0:
+            if cadena1[c]==cadena2[0]:
+                while es < len(cadena2):
+                    if cadena1[c+desplaza]==cadena2[es]:
+                        es+=1
+                        desplaza+=1
+                if es == len(cadena2) and cadena1[c+len(cadena2)]==" ":
+                    nuevafrase=nuevafrase + cadena3
+                    c+=len(cadena2)
+                    es=0
+                else:
+                    nuevafrase="La palabra a sustituir no se encuentra"
+                    es=0
+                    c+=1
+                    control=1
+            else:
+                nuevafrase+=cadena1[c]
+                c+=1
+    return nuevafrase
+
+print(reemplazarPalabra(cadena1,cadena2,cadena3))
--- python/Funciones/modular3/Ejercicio_8.py
+++ python/Funciones/modular3/Ejercicio_8.py
@@ -1,15 +1,15 @@
 '''
-Created on Nov 29, 2022
+Created on 29 nov 2022
 
-@author: alfonso
+@author: alfon
 '''
 string=str(input("Enter a phrase you want:"))
 def contar_vocales(string):
-    contador = 0
+    contador = []
     for letter in string:
-        if letter.lower() in "aeiou":
-            contador += 1
-    return contador
+        if letter.lower() in "aeiou"  and letter.lower() not in contador:
+            contador += letter
+    return len (contador)
 
 amount = contar_vocales(string)
 print(f"In the chain '{string}' have {amount} vowels")
